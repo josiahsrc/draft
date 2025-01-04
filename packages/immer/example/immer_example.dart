a5dccadbabc8ee9317dbba869d3c3b28b47a732e
Josiah Saunders <contact@josiahsaunders.com>
2025-01-04 12:26:32 -0700

    prototype for how it would work

---
+++ packages/immer/example/immer_example.dart
@@ -0,0 +1,10 @@
+import 'package:immer/immer.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
+
+class Something { 
+
+}
---
+++ packages/immer/lib/src/immer_base.dart
@@ -0,0 +1,17 @@
+// TODO: Put public facing types in this file.
+
+import 'package:macros/macros.dart';
+
+/// Checks if you are awesome. Spoiler: you are.
+class Awesome {
+  bool get isAwesome => true;
+}
+
+// macro class Hello implements ClassDeclarationsMacro {
+//   const Hello();
+
+//   @override
+//   void buildDeclarationsForClass(ClassDeclaration clazz, MemberDeclarationBuilder builder) {
+//     print('Hello, World');
+//   }
+// }
