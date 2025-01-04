f9338e28d530d7ab4eb64128836a728eb8806618
Josiah Saunders <contact@josiahsaunders.com>
2025-01-04 13:14:56 -0700

    basic macro

--- packages/draft/example/draft_example.dart
+++ packages/draft/example/draft_example.dart
@@ -5,5 +5,29 @@ void main() {
   print('awesome: ${awesome.isAwesome}');
 }
 
-@Hello()
-class Something {}
+@Draft()
+class Foo {
+  final String fieldA;
+  final String fieldB;
+
+  const Foo({
+    required this.fieldA,
+    required this.fieldB,
+  });
+}
+
+void testIt() {
+  final foo = Foo(fieldA: 'a', fieldB: 'b');
+
+  final foo2 = foo.produce((draft) {
+    draft.fieldB = 'c';
+  });
+
+  print(foo2.fieldB); // c
+
+  final foo5 = foo.draft();
+  foo5.fieldB = 'd';
+  foo5.fieldA = 'e' * 5;
+
+  print(foo5.save());
+}
--- packages/draft/lib/src/draft_base.dart
+++ packages/draft/lib/src/draft_base.dart
@@ -1,5 +1,7 @@
 // TODO: Put public facing types in this file.
 
+import 'dart:async';
+
 import 'package:macros/macros.dart';
 
 /// Checks if you are awesome. Spoiler: you are.
@@ -7,24 +9,71 @@ class Awesome {
   bool get isAwesome => true;
 }
 
-macro class Hello implements ClassDeclarationsMacro {
-  const Hello();
+macro class Draft implements ClassDeclarationsMacro, ClassTypesMacro {
+  const Draft();
 
   @override
   Future<void> buildDeclarationsForClass(
     ClassDeclaration clazz,
     MemberDeclarationBuilder builder,
   ) async {
-    // final fields = await builder.fieldsOf(clazz);
+    final fields = await builder.fieldsOf(clazz);
     // final fieldsString = fields.map((f) => f.identifier.name).join(', ');
 
-    builder.declareInType(
-      DeclarationCode.fromParts([
-        'void hello2()',
-        '{',
-        '  print("Hello, World!");',
+    // builder.declareInLibrary(DeclarationCode.fromParts([
+    //   'class ${clazz.identifier.name}Draft {',
+    //   for (final field in fields)
+    //     '${field.type} ${field.identifier};',
+    //   '}',
+    // ]));
+
+  //   SomeObject produce(void Function(SomeObjectDraft draft) fn) {
+  //   final draft = this.draft();
+  //   fn(draft);
+  //   return draft.save();
+  // }
+
+    final name = '${clazz.identifier.name}Draft';
+    builder.declareInType(DeclarationCode.fromParts([
+        // 'void hello3()',
+        // '{',
+        // '  print("Hello, World!");',
+        // '}',
+        '$name draft() {',
+        '  return $name(',
+        for (final field in fields)
+          '${field.identifier.name}: ${field.identifier.name},',
+        ');',
         '}',
-      ]),
-    );
+        '${clazz.identifier.name} produce(void Function($name draft) fn) {',
+        '  final draft = this.draft();',
+        '  fn(draft);',
+        '  return draft.save();',
+        '}',
+      ]),);
+  }
+
+  @override
+  FutureOr<void> buildTypesForClass(ClassDeclaration clazz, ClassTypeBuilder builder,) async {
+    // final fields = await clazz.
+    final fields = ['fieldA', 'fieldB'];
+
+    final name = '${clazz.identifier.name}Draft';
+    builder.declareType(name, DeclarationCode.fromParts([
+      'class $name {',
+      for (final field in fields)
+        'String ${field};',
+      '$name({'
+      'required this.fieldA,'
+      'required this.fieldB,'
+      '});',
+      '${clazz.identifier.name} save() {',
+      '  return ${clazz.identifier.name}(',
+      for (final field in fields)
+        '${field}: ${field},',
+      ');',
+      '}',
+      '}',
+    ]));
   }
 }
