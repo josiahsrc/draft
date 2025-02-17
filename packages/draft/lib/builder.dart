47edb063a11367748e9c7b87d3bbbcc62cd62aa1
Josiah Saunders <contact@josiahsaunders.com>
2025-02-16 21:03:08 -0700

    rename

--- packages/draft/lib/builder.dart
+++ packages/draft/lib/builder.dart
@@ -1,6 +1,6 @@
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
-import 'src/draft_generator.dart';
+import 'src/generator.dart';
 
 Builder draftBuilder(BuilderOptions options) {
   return PartBuilder(
