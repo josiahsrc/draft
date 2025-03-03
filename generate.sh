798baf8b70de6af976fd9cea10c3b05367c9c42e
Josiah Saunders <contact@josiahsaunders.com>
2025-03-02 20:02:29 -0700

    fix builder scripts

--- generate.sh
+++ generate.sh
@@ -5,5 +5,5 @@ gen() {
   dart run build_runner build --delete-conflicting-outputs
 }
 
-(cd packages/draft && gen)
-(cd packages/draft/example && gen)
+(cd packages/draft_builder && gen)
+(cd packages/draft_builder/example && gen)
--- packages/draft/build_runner.sh
+++
@@ -1,3 +0,0 @@
-# run dart build runner
-cd "$(dirname "$0")"
-dart run build_runner build --delete-conflicting-outputs
